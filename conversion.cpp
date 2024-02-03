e13d331b9d51d953d704fa40837dccd6962ab318
siddiq2772 <siddiqmohd2772@gmail.com>
2024-02-03 20:14:53 +0530

    c++

--- conversion.cpp
+++ conversion.cpp
@@ -10,7 +10,7 @@ public:
     void getbinary() {
           cout << "Enter a binary number:";
         cin >> bin;
-        
+        // hello
         for (i = 0; bin != 0; i++) {
             dec = dec + pow(2, i) * (bin % 10);
             bin = bin / 10;
